c56b7a57e9cd156ad67425dcd8ac6660acb7d81f
mickamy <tetsuro@mickamy.com>
2024-06-01 22:10:39 +0900

    learn: done 2.2

---
+++ programming-lang-go/ch02/ftoc/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("freezing point = %g°F or %g°C\n", freezingF, fToC(freezingF))
+	fmt.Printf("boiling point = %g°F or %g°C\n", boilingF, fToC(boilingF))
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
